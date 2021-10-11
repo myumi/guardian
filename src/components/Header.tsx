126998a243f8f3636a9826c39a2e10fa7c61ec12
Mayumi <myumiak@gmail.com>
2021-10-11 14:46:12 -0400

    header redesign

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import { ReactComponent as DiceSVG } from '../assets/header/dice.svg';
+import { ReactComponent as HeartSVG } from '../assets/header/heart.svg';
+import { ReactComponent as FaqSVG } from '../assets/header/faq.svg';
+import { ReactComponent as GearSVG } from '../assets/header/gear.svg';
 import '../styles/Header.scss';
 
 export default function Header() {
   return (
     <header className="header">
+      <div className="header__content">
       <h1 className="header__title">Guardian</h1>
-      <nav className="header__nav"></nav>
+
+      <nav className="header__nav">
+        <div className="header__nav-item">
+          <DiceSVG />
+          <span className="header__nav-item__label">Breeding Calculator</span>
+        </div>
+        <div className="header__nav-item">
+          <HeartSVG />
+          <span className="header__nav-item__label">Matchmaking</span>
+        </div>
+        <div className="header__nav-item">
+          <FaqSVG />
+          <span className="header__nav-item__label">FAQ</span>
+        </div>
+        <div className="header__nav-item">
+          <GearSVG />
+          <span className="header__nav-item__label">Settings</span>
+        </div>
+      </nav>
+
+      {/* <div className="header__settings">
+        <GearSVG />
+        <span className="settings__label">Settings</span>
+      </div> */}
+      </div>
     </header>
-  )
-}
+  );
+};
